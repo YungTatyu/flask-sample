e0b5a079a794da2f82f736bcf14985fc9e54d63b
tachu <roatyungtatyu@gmail.com>
2025-01-31 00:51:16 +0900

    add: db setting

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@ import os
 
 from flask import Flask
 
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
 
 def create_app(test_config=None):
     # create and configure the app
@@ -13,10 +17,12 @@ def create_app(test_config=None):
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
+        app.config.from_object("app.config")
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
+
+    db.init_app(app)
 
     # ensure the instance folder exists
     try:
