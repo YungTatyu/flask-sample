02e1d816c55267519af20af8b54d091acc2c96c6
tachu <roatyungtatyu@gmail.com>
2025-01-31 11:25:58 +0900

    add: users post test

--- tests/views/test_user_views.py
+++ tests/views/test_user_views.py
@@ -1,4 +1,7 @@
+from os import name
 import pytest
+from app.models.user_model import User
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 def test_request_signup(client):
@@ -10,13 +13,49 @@ def test_request_signup(client):
 class TestUsersAPI:
     """userAPIのテスト"""
 
-    def test_edit_user(self, client):
+    @pytest.fixture(autouse=True)
+    def setup(self, db_session):
+        """テストの前にユーザーをセットアップ"""
+        self.user = User(name="test user", password="password", email="test@test.com")
+        db_session.add(self.user)
+        db_session.commit()
+
+    def assert_response(self, expected_keys, response):
+        for key in expected_keys:
+            assert key in response
+
+    def assert_db_data(self, actual, expect):
+        assert actual.id == expect.id
+        assert actual.name == expect.name
+        assert actual.email == expect.email
+        assert check_password_hash(actual.password, expect.password)
+        assert actual.is_active == expect.is_active
+
+    def test_create_user(self, client, db_session):
+        NAME = "test user"
+        PASSWORD = "password"
+        EMAIL = "test@example.com"
         response = client.post(
             "/users",
             json={
-                "name": "test user",
-                "password": "password",
-                "email": "test@example.com",
+                "name": NAME,
+                "password": PASSWORD,
+                "email": EMAIL,
             },
         )
         assert response.status_code == 201
+
+        response_json = response.get_json()
+        self.assert_response(["id", "name", "email", "is_active"], response_json)
+
+        actual = db_session.query(User).filter_by(id=response_json["id"]).first()
+        self.assert_db_data(
+            actual,
+            User(
+                id=response_json["id"],
+                name=NAME,
+                email=EMAIL,
+                password=PASSWORD,
+                is_active=True,
+            ),
+        )
