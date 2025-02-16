f37fb66313ed349fab0a0c9a5b5b98bb5789c865
tachu <roatyungtatyu@gmail.com>
2025-02-16 15:44:16 +0900

    add: .env

--- app/config.py
+++ app/config.py
@@ -1,5 +1,9 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Flaskの設定
--- app/views/auth_views.py
+++ app/views/auth_views.py
@@ -1,9 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, render_template, request
 
 
 auth_bp = Blueprint("auth", __name__)
 
 
-@auth_bp.route("/login", methods=["GET"])
+@auth_bp.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+
+    data = request.form
+    email = data.get("email")
+    password = data.get("password")
+    if not email or not password:
+        flash("Missing required fields", "error")
+        return render_template("login.html"), 400
+    return render_template("home.html")
